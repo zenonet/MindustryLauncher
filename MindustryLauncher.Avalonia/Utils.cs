94c1559ef83ca96656c08da1ba454c440679d8fa
zenonet <78077158+zenonet@users.noreply.github.com>
2023-12-06 21:25:43 +0100

    Awaited usings and made the statement like

--- MindustryLauncher.Avalonia/Utils.cs
+++ MindustryLauncher.Avalonia/Utils.cs
@@ -11,40 +11,38 @@ public static class Utils
     public static async Task DownloadFile(this HttpClient client, string url, string destinationPath,
         IProgress<float>? progress = null)
     {
-        using (HttpResponseMessage response = client.GetAsync(url, HttpCompletionOption.ResponseHeadersRead).Result)
+        using HttpResponseMessage response = client.GetAsync(url, HttpCompletionOption.ResponseHeadersRead).Result;
+        response.EnsureSuccessStatusCode();
+
+        await using Stream contentStream = await response.Content.ReadAsStreamAsync(),
+            fileStream = File.Create(destinationPath);
+        
+        long totalRead = 0L;
+        long totalReads = 0L;
+        byte[] buffer = new byte[8192];
+        bool isMoreToRead = true;
+
+        float contentLength = (float) response.Content.Headers.ContentLength!;
+
+        do
         {
-            response.EnsureSuccessStatusCode();
-
-            using (Stream contentStream = await response.Content.ReadAsStreamAsync(),
-                   fileStream = File.Create(destinationPath))
+            int read = await contentStream.ReadAsync(buffer, 0, buffer.Length);
+            if (read == 0)
             {
-                long totalRead = 0L;
-                long totalReads = 0L;
-                byte[] buffer = new byte[8192];
-                bool isMoreToRead = true;
-
-                float contentLength = (float) response.Content.Headers.ContentLength!;
-
-                do
-                {
-                    int read = await contentStream.ReadAsync(buffer, 0, buffer.Length);
-                    if (read == 0)
-                    {
-                        isMoreToRead = false;
-                    }
-                    else
-                    {
-                        await fileStream.WriteAsync(buffer, 0, read);
-
-                        totalRead += read;
-                        totalReads += 1;
-
-                        progress?.Report(totalRead / contentLength * 100);
-                    }
-                } while (isMoreToRead);
-                fileStream.Close();
+                isMoreToRead = false;
             }
-        }
+            else
+            {
+                await fileStream.WriteAsync(buffer, 0, read);
+
+                totalRead += read;
+                totalReads += 1;
+
+                progress?.Report(totalRead / contentLength * 100);
+            }
+        } while (isMoreToRead);
+
+        fileStream.Close();
     }
 
     public static string GetSha256OfFile(string path)
@@ -53,7 +51,7 @@ public static class Utils
         using FileStream fileStream = File.OpenRead(path);
         return Convert.ToBase64String(sha256.ComputeHash(fileStream));
     }
-    
+
     public static bool VerifyIntegrity(this ILocalInstance instance)
     {
         string jarPath = instance.JarPath;
